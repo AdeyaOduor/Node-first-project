55883d0f2977718ade722fdc265ecc8e14d845bc
anubhab137 <adamsanubhab@gmail.com>
2020-12-11 23:01:40 +0530

    Added err throw

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs=require('fs');
 http.createServer((req,res)=>{
     if(req.url==='/'){
         fs.readFile('index.html',function(err,data){
+            if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
@@ -11,6 +12,7 @@ http.createServer((req,res)=>{
     }
     else if(req.url==='/about'){
         fs.readFile('about.html',function(err,data){
+            if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
@@ -18,6 +20,7 @@ http.createServer((req,res)=>{
     }
     else if(req.url==='/contact-me'){
         fs.readFile('contact-me.html',function(err,data){
+            if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
@@ -25,11 +28,13 @@ http.createServer((req,res)=>{
     }
     else{
         fs.readFile('404.html',function(err,data){
+            if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end;
         });
     }
-}).listen(6969);
+}).listen(6969,()=>{
+    console.log('Listeninig on port  6969.....');
+});
 
-console.log('Listeninig on port  6969.....');
